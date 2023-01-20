1f41ca9fe23d8130ffe096dd45741cc4baa5cf9f
ashwanikarma <ashwanikarma9@gmail.com>
2023-01-20 14:10:03 +0530

    [Ashwani] - Added all three condition to one generic class

--- GenericsTestMaximum/Program.cs
+++ GenericsTestMaximum/Program.cs
@@ -15,6 +15,12 @@ namespace GenericsTestMaximum
             Console.WriteLine("Enter Three string Numbers : 50.1,90.3,10.7");
             Console.WriteLine(MaximumGenericMethod.MaximumIntegerNumber("50.1", "90.3", "10.7"));
 
+            Console.WriteLine("_______________________________________________________________________");
+            Console.WriteLine("Maximum using Generic Class");
+            Console.WriteLine("Maximum For Integer " + MaximumGenericClass<int>.MaxValue(100, 115, 211));
+            Console.WriteLine("Maximum For Float " + MaximumGenericClass<float>.MaxValue(19.1f, 27.9f, 18.7f));
+            Console.WriteLine("Maximum For String " + MaximumGenericClass<string>.MaxValue("ash", "wani", "karma"));
+
         }
     }
 }
