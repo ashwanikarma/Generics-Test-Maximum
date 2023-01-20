b68da40866090179b1bd755293d71fe268dda3a7
ashwanikarma <ashwanikarma9@gmail.com>
2023-01-20 14:02:03 +0530

    [Ashwani] - Added all three condition to one generic method

--- GenericsTestMaximum/Program.cs
+++ GenericsTestMaximum/Program.cs
@@ -5,27 +5,15 @@ namespace GenericsTestMaximum
 {
     internal class Program
     {
-        public static string MaximumIntegerNumber(string firstValue, string secondValue, string thirdValue)
-        {
-            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
-            {
-                return firstValue;
-            }
-            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
-            {
-                return secondValue;
-            }
-            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
-            {
-                return thirdValue;
-            }
-            return firstValue;
-        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to program for Generics Test Maximum of Three number");
-            Console.WriteLine("Enter Three Numbers : 50.1,90.3,10.7");
-            Console.WriteLine(MaximumIntegerNumber("50.1", "90.3", "10.7"));
+            Console.WriteLine("Enter Three int Numbers : 50,90,10");
+            Console.WriteLine(MaximumGenericMethod.MaximumIntegerNumber(50, 90, 10));
+            Console.WriteLine("Enter Three float Numbers : 50.1,90.3,10.7");
+            Console.WriteLine(MaximumGenericMethod.MaximumIntegerNumber(50.1, 90.3, 10.7));
+            Console.WriteLine("Enter Three string Numbers : 50.1,90.3,10.7");
+            Console.WriteLine(MaximumGenericMethod.MaximumIntegerNumber("50.1", "90.3", "10.7"));
 
         }
     }
